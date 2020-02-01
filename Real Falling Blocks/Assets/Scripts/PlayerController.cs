0ad3efcd082e72669ffd2664fde0492e8aae3e0a
Adam Farley <adamfrly@gmail.com>
2020-02-01 17:48:10 -0500

    initial commit

---
+++ Real Falling Blocks/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 7;
+    float halfScreenWidth;
+    public event System.Action OnPlayerDeath;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        halfScreenWidth = Camera.main.aspect * Camera.main.orthographicSize + transform.localScale.x / 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inputX = Input.GetAxisRaw("Horizontal");
+        float velocity = inputX * speed;
+        transform.Translate(Vector2.right * velocity * Time.deltaTime);
+
+        if(transform.position.x < -halfScreenWidth)
+        {
+            transform.position = new Vector2(halfScreenWidth, transform.position.y);
+        }
+        if (transform.position.x > halfScreenWidth)
+        {
+            transform.position = new Vector2(-halfScreenWidth, transform.position.y);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D triggerCollider)
+    {
+        if (triggerCollider.tag == "FallingBlock")
+        {
+            if(OnPlayerDeath != null)
+            {
+                OnPlayerDeath();
+            }
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Stealth Game/Assets/Scripts/Player.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public event System.Action OnReachedEndOfLevel;
+
+    public float movespeed = 7;
+    public float smoothMoveTime = 0.1f;
+    public float turnSpeed = 8;
+
+    float angle;
+    float smoothInputMagnitude;
+    float smoothMoveVelocity;
+    Vector3 velocity;
+
+    Rigidbody rigidbody;
+    bool disabled;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        Guard.OnGuardHasSpottedPlayer += Disable;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 inputDirection = Vector3.zero;
+        if (!disabled)
+        {
+            inputDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+        }
+        float inputMagnitude = inputDirection.magnitude;
+        smoothInputMagnitude = Mathf.SmoothDamp(smoothInputMagnitude, inputMagnitude, ref smoothMoveVelocity, smoothMoveTime);
+
+        float targetAngle = Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg;
+        angle = Mathf.LerpAngle(angle, targetAngle, Time.deltaTime * turnSpeed * inputMagnitude);
+
+        velocity = transform.forward * movespeed * smoothInputMagnitude;
+    }
+
+    void OnTriggerEnter(Collider hitCollider)
+    {
+        if(hitCollider.tag == "Finish")
+        {
+            Disable();
+            OnReachedEndOfLevel?.Invoke();
+        }
+    }
+
+    void Disable()
+    {
+        disabled = true;
+    }
+
+    void FixedUpdate()
+    {
+        rigidbody.MoveRotation(Quaternion.Euler(Vector3.up * angle));
+        rigidbody.MovePosition(rigidbody.position + velocity * Time.deltaTime);
+        
+    }
+
+    void OnDestroy()
+    {
+        Guard.OnGuardHasSpottedPlayer -= Disable;    
+    }
+}
